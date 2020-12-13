6bf7e022ab4efa3c53d595eb8a610ba9ea3a60b5
Jelle Schutter <jelle@schutter.xyz>
2020-12-13 14:21:29 +0100

    bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='wikidata_plain_sparql',
-    version='0.0.3',
+    version='0.0.4',
     author='Jelle Schutter',
     author_email='jelle@schutter.xyz',
     description='Query WikiData in plain SPARQL',
